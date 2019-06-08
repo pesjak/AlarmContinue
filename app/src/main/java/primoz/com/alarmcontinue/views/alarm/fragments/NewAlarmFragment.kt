b30d1bf2574b264e94cc6624c79659169a271325
WinterChilly <primoz.pesjak@gmail.com>
2019-06-08 12:42:41 +0200

    XML started for Bed
    - Hide keyboard ugly hack
    - STYLE for select time

--- app/src/main/java/primoz/com/alarmcontinue/views/alarm/fragments/NewAlarmFragment.kt
+++ app/src/main/java/primoz/com/alarmcontinue/views/alarm/fragments/NewAlarmFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_new_alarm.*
 import primoz.com.alarmcontinue.R
 
 class NewAlarmFragment : Fragment() {
@@ -15,6 +16,8 @@ class NewAlarmFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        timePicker.setIs24HourView(true)
     }
 
 
